0a1f91dff373b0a5be1991176ba9a42ea1697a36
Linozduart <42115827+Linozduart@users.noreply.github.com>
2023-04-24 23:42:49 -0300

    Create 84.py

---
+++ Desafios Python/84.py
@@ -0,0 +1,24 @@
+pessoas = []
+pessoa_pesada = {'Nome': '', 'Peso': 0}
+pessoa_leve = {'Nome': '', 'Peso': 0}
+soma_idades = 0
+
+for control in range(2):
+    pessoa = {
+        'Nome': input('Qual é o seu nome?: '),
+        'Idade': int(input('Qual é a sua idade?: ')),
+        'Peso': float(input('Qual é o seu peso?: '))
+    }
+    print(f'Pessoa {control} foi registrada')
+    soma_idades = pessoa['Idade']
+    pessoas.append(pessoa)
+    if pessoa['Peso'] > pessoa_pesada['Peso']:
+        pessoa_pesada = pessoa
+    else:
+        pessoa_leve = pessoa
+print(f'Foram registradas {len(pessoas)} pessoas')
+print(
+    f'{pessoa_pesada["Nome"]} é a pessoa mais pesada, com {pessoa_pesada["Peso"]} kg.')
+print(
+    f'{pessoa_leve["Nome"]} é a pessoa mais leve, com {pessoa_leve["Peso"]} kg')
+print(f'A soma das idades de todas as pessoas é {soma_idades}')
