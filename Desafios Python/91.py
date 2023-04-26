83028ec38077ec67d9c6eae0ac7fcb7bc7faf7ed
Linozduart <42115827+Linozduart@users.noreply.github.com>
2023-04-26 17:38:17 -0300

    Update 91.py

--- Desafios Python/91.py
+++ Desafios Python/91.py
@@ -1,24 +1,29 @@
 from random import randint
 from time import sleep
+from os import system, name
+
 
 jogadores = {}
 
-print('-'*30)
+system('clear') if name == 'posix' else system('cls')
 
-for i in range(4):
+for i in range(1, 5):
     dado = randint(1, 6)
-    jogador = i + 1
-    print(f'O jogador {jogador} tirou {dado}')
-    jogadores[jogador] = {
-        'jogador': jogador,
-        'dado': dado
+    jogadores[i] = {
+        'jogador': i, 'dado': dado
     }
+
+    print(f'O jogador {i} tirou {dado}')
     sleep(0.5)
+
 print('-'*30)
 
-def ranking():
-    print('Ranking dos Jogadores:')
-    jogadores_ordenados = sorted(jogadores.values(), key=lambda jogador: jogador['dado'], reverse=True)
-    for i, jogador in enumerate(jogadores_ordenados):
-        posicao = i + 1
-        print(f'{posicao}º lugar: Jogador {jogador["jogador"]} com {jogador["dado"]}')
+print('Ranking dos Jogadores:')
+
+jogadores = sorted(
+    jogadores.values(),
+    key=lambda jogador: jogador['dado'],
+    reverse=True)
+for i, jogador in enumerate(jogadores):
+    print(f'{i+1}º lugar: Jogador {jogador["jogador"]} com {jogador["dado"]}')
+
