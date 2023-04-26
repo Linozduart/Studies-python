d6aaa24d602fa8f51a4e5aeef5a5e17a314d549c
Linozduart <42115827+Linozduart@users.noreply.github.com>
2023-04-25 23:50:23 -0300

    Update 89.py

--- Desafios Python/89.py
+++ Desafios Python/89.py
@@ -5,15 +5,16 @@ except:
     quantidade_alunos = int(input('Quantos alunos serão registrados?: '))
 
 alunos = []
-for i in range(quantidade_alunos):
+while True:
     aluno = {
         'Nome': input('Digite o nome do aluno: '),
-        'Nota1': int(input('Digite a nota do aluno: ')),
-        'Nota2': int(input('Digite a nota do aluno: '))
+        'Nota1': float(input('Digite a nota do aluno: ')),
+        'Nota2': float(input('Digite a nota do aluno: '))
     }
     alunos.append(aluno)
     conti = input('Deseja registrar mais? (s/n): ')
     if conti == 'n':
         break
 print('-'*15)
-print()
+for aluno in alunos:
+    print(f'0 {aluno["Nome"]} sua média foi {aluno["Nota1"]/aluno["Nota2"]:.2f}')
