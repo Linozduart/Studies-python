2e8512cd3a60d2cb7d812ac9ced788aa2dec0f2b
Indra <42115827+Linozduart@users.noreply.github.com>
2023-04-28 18:10:02 -0300

    Update 92.py

--- Desafios Python/92.py
+++ Desafios Python/92.py
@@ -14,15 +14,28 @@ data_nascimento = datetime.datetime.strptime(dados['nascimento'], "%d/%m/%Y")
 # ((today.month, today.day) < (data_nascimento.month, data_nascimento.day))
 idade = today.year - data_nascimento.year
 dados['idade'] = idade
+
 if dados['clt'] == 'sim':
-    data_clt = input('Qual ano de contratação? (dd/mm/aaaa)')
+    data_clt = input('Qual ano de contratação(dd/mm/aaaa)?: ')
     salario = float(input('Qual seu salario: '))
-    data_contratação = datetime.datetime.strptime(data_clt, "%d/%m/%Y")
-    dados['data_contratacao'] = print(
-        f'Voce à {data_clt.year - (-today.year)} ')
+    data_contratacao = datetime.datetime.strptime(data_clt, "%d/%m/%Y")
+    dados['data_contratacao'] = data_contratacao.year - (today.year)
     dados['salario'] = salario
+
 if dados['sexo'] == 'm':
-    tempo_aposentar = dados['idade'] - (-65)
 
+    tempo_aposentar = abs(dados['idade'] - 65)
+    dados['tempo_aposentar_M'] = tempo_aposentar
+    print(
+        f'Olá {dados["nome"]},vi que você trabalha à {dados["data_contratacao"]} anos.')
+    print(
+        f'Você tem {dados["idade"]} anos e faltam {dados["tempo_aposentar_M"]} anos para você se aposentar')
 
-print(dados)
+else:
+
+    tempo_aposentar = abs(dados['idade'] - 62)
+    dados['tempo_aposentar_F'] = tempo_aposentar
+    print(
+        f'Olá {dados["nome"]},vi que você trabalha à {dados["data_contratacao"]} anos.')
+    print(
+        f'Você tem {dados["idade"]} anos e faltam{dados["tempo_aposentar_F"]} anos para você se aposentar')
