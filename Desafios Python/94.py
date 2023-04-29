44b452eb1f932e866be01cfb35de38c5ea7cbbe8
Indra <42115827+Linozduart@users.noreply.github.com>
2023-04-28 23:41:06 -0300

    Update 94.py

--- Desafios Python/94.py
+++ Desafios Python/94.py
@@ -1,15 +1,28 @@
-
 qnt_user = int(input('Quantas pessoas deseja registrar: '))
-
-for i in range(qnt_user):
-    
 
 users = []
 
-user = {
-    'nome':,
-    'sexo':,
-    'idade':
-}
+for i in range(qnt_user):
+
+    users.append({
+        'nome': input('Qual seu nome: '),
+        'sexo': input('Qual seu gênero (M/F): ').lower(),
+        'idade': int(input('Qual sua idade ?: '))
+    })
+
+print(f'Foram cadastradas {qnt_user} pessoas')
 
 
+def adicionais():
+    soma = 0
+    woman = list()
+    medialist = list()
+    for user in users:
+        soma += user["idade"]
+    print(f'A média de idades é {soma/len(users)}')
+    if user["sexo"] == 'f':
+        woman.append(user)
+    print(f'{woman}')
+    if user["idade"] > soma/len(users):
+        medialist.append(user)
+adicionais()
