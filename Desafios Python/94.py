36f955d0212e657cee7208336009e7196df3b518
Indra <42115827+Linozduart@users.noreply.github.com>
2023-04-28 23:42:45 -0300

    Update 94.py

--- Desafios Python/94.py
+++ Desafios Python/94.py
@@ -15,14 +15,17 @@ print(f'Foram cadastradas {qnt_user} pessoas')
 
 def adicionais():
     soma = 0
-    woman = list()
-    medialist = list()
+    woman = []
+    medialist = []
     for user in users:
         soma += user["idade"]
+        if user["sexo"] == 'f':
+            woman.append(user)
+        if user["idade"] > soma/len(users):
+            medialist.append(user)
     print(f'A média de idades é {soma/len(users)}')
-    if user["sexo"] == 'f':
-        woman.append(user)
-    print(f'{woman}')
-    if user["idade"] > soma/len(users):
-        medialist.append(user)
+    print(f'Mulheres cadastradas: {woman}')
+    print(f'Pessoas com idade acima da média: {medialist}')
+
+
 adicionais()
