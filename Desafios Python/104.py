da11f2cf67959bcee37ab4defd36cecf77a45489
Indra <42115827+Linozduart@users.noreply.github.com>
2023-04-29 21:35:54 -0300

    Update

--- Desafios Python/104.py
+++ Desafios Python/104.py
@@ -1,5 +1,10 @@
+
 def leia(q):
     numero = input(q)
+    while not numero.isnumeric():
+        print(f'\033[31mDigite um número!')
+        numero = input(q)
     print(f'Você acabou de digitar o número {numero}')
+
 
 q = leia('Digite um número: ')
---
+++ Desafios Python/105.py
@@ -0,0 +1,23 @@
+def notas(*n, sit=False):
+
+    dados = {
+        'total': len(n),
+        'maior': max(n),
+        'menor': min(n),
+        'média': sum(n)/len(n),
+
+    }
+    if sit == True:
+        dados["situação"] = None
+        if dados["média"] >= 8:
+            dados["situação"] = 'Ótima'
+        elif dados["média"] >= 6:
+            dados["situação"] = 'Boa'
+        else:
+            dados["situação"] = 'Ruim'
+
+    return dados
+
+
+resp = notas(7.5, 9.5, 10, 6.5, sit=True)
+print(resp)
